164dfc7058a29dc2d2c4d8c71308479e83c3df0d
FernandoGalende <fernandogalende@gmail.com>
2018-05-08 15:59:19 +0200

    first commit v2

---
+++ client/src/app/auth-signup/auth-signup.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { SessionService } from "../session.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-auth-signup",
+  templateUrl: "./auth-signup.component.html",
+  styleUrls: ["./auth-signup.component.css"]
+})
+export class AuthSignupComponent implements OnInit {
+  username: string;
+  password: string;
+  name: string;
+  secret: string;
+
+  constructor(public session: SessionService, public router: Router) {}
+
+  ngOnInit() {}
+  signup() {
+    const user = {
+      username: this.username,
+      password: this.password,
+      name: this.name,
+      secret: this.secret
+    };
+    this.session.signup(user).subscribe(() => this.router.navigate(["/"]));
+  }
+}
---
+++ client/src/app/my-private-page/my-private-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from "./../session.service";
+
+@Component({
+  selector: 'app-my-private-page',
+  templateUrl: './my-private-page.component.html',
+  styleUrls: ['./my-private-page.component.css']
+})
+export class MyPrivatePageComponent implements OnInit {
+
+
+  constructor(public session: SessionService) { }
+
+  ngOnInit() {
+  }
+}
