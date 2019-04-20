fb369503e062e5028f94236efe056dbfc8add50e
FernandoGalende <fernandogalende@gmail.com>
2019-04-20 19:59:22 +0200

    change health route name

--- server_refactor/src/routes/routes.js
+++ server_refactor/src/routes/routes.js
@@ -4,8 +4,7 @@ const { health } = require('./health')
 const appRouter = (app) => {
   app.get('/fonts', (req, res, next) => fontRoutes.getListOfFonts(res, next))
   app.get('/font/:id', (req, res, next) => fontRoutes.getSingleFont(req, res, next))
-  app.get('/', (req, res, next) => health(req, res, next))
-  app.get('*', (req, res, next) => next({ status: 404, message: 'The route does not exist' }))
+  app.get('/health', (req, res, next) => health(req, res, next))
 }
 
 module.exports = appRouter
